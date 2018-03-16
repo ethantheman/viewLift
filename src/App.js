d30a79986c283e15056f1ced4a01f40f838c76be
Ethan Lipkind <Ethan.lipkind@gmail.com>
2018-03-16 10:15:52 -0400

    deploy

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import git from './images/git.png'
 import loading from './images/loading.gif';
 import linkedin from './images/linkedin.png';
 import './App.css';
+const data = require('./data.js');
 const axios = require('axios');
 
 class App extends Component {
@@ -29,26 +30,26 @@ class App extends Component {
   }
 
   componentDidMount() {
-    axios.get('http://www.snagfilms.com/apis/films.json?limit=10').then(obj => {
+    axios.get('https://www.snagfilms.com/apis/films.json?limit=10').then(obj => {
       this.setState({
-        loading: false,
         films: obj.data.films.film,
         categoryOne: obj.data.films.film.slice(0, 7),
         categoryTwo: obj.data.films.film.slice(0, 7),
         categoryThree: obj.data.films.film.slice(0, 7),
-        categoryFour: obj.data.films.film.slice(0, 7)
-
-      });
-      setInterval(() => {
-        if ( this.state.swiping ) {
-          if ( this.state.films && this.state.idx === this.state.films.length - 1) {
-            this.setState({idx: 0});
-          } else {
-            let i = this.state.idx;
-            this.setState({idx: i+1});
+        categoryFour: obj.data.films.film.slice(0, 7),
+        loading: false
+      }, () => {
+        setInterval(() => {
+          if ( this.state.swiping ) {
+            if ( this.state.films && this.state.idx === this.state.films.length - 1) {
+              this.setState({idx: 0});
+            } else {
+              let i = this.state.idx;
+              this.setState({idx: i+1});
+            }
           }
-        }
-      }, 4000);
+        }, 4000);
+      });
     });
   }
 
@@ -111,7 +112,7 @@ class App extends Component {
           <div className="header-text-2">MOVIES</div>
           <div className="header-text-3">SHOWS</div>
           <div className="filler"></div>
-          <img className="search" src={search}></img>
+          <img className="search" src={search} alt="search"></img>
           <div className="login">LOGIN</div>
         </header>
         <div className="gallery-container">
@@ -121,36 +122,36 @@ class App extends Component {
               <div id="gallery-description">{this.state.films[this.state.idx].title}</div>
               <button id="watch">Watch Now</button>
               <div id="buttonContainer">
-                <img id="prev" src={prev} onClick={this.prev}/>
+                <img id="prev" src={prev} onClick={this.prev} alt="prev"/>
                 {/*need to add circles in here somehow...*/}
-                <img id="next" src={next} onClick={this.next}/>
+                <img id="next" src={next} onClick={this.next} alt="next"/>
               </div>
-              <img className="gallery-photo" src={this.state.films[this.state.idx].images.image[0].src}/>
+              <img className="gallery-photo" src={this.state.films[this.state.idx].images.image[0].src} alt="gallery" onError={() => {alert('Something on the browser is blocking an image. Perhaps adblocker?')}}/>
             </div>
           }
         </div>
         <h3>SAMPLE CATEGORY</h3>
-        <div className="category"><img src={prev} id="category-prev" onClick={this.onePrev}/>{this.state.categoryOne ? this.state.categoryOne.map((film, i) => {
-          return (<div className="card" key={i} style={{"color": "white"}}><div className="play"><img src="http://cdn1.iconfinder.com/data/icons/flavour/button_play_blue.png" /> </div><img className="category-photo" src={film.images.image[0].src}/>{film.title}</div>)
-        }) : null}<img src={next} id="category-next" onClick={this.oneNext}/></div>
+        <div className="category"><img src={prev} id="category-prev" onClick={this.onePrev} alt="prev"/>{this.state.categoryOne ? this.state.categoryOne.map((film, i) => {
+          return (<div className="card" key={i} style={{"color": "white"}}><div className="play"><img src="http://cdn1.iconfinder.com/data/icons/flavour/button_play_blue.png" alt="" /> </div><img className="category-photo" src={film.images.image[0].src}/>{film.title}</div>)
+        }) : null}<img src={next} id="category-next" onClick={this.oneNext} alt="next"/></div>
         <h3>SAMPLE CATEGORY</h3>
         <div className="category"><img src={prev} id="category-prev" onClick={this.twoPrev}/>{this.state.categoryTwo ? this.state.categoryTwo.map((film, i) => {
-          return (<div className="card" key={i} style={{"color": "white"}}><div className="play"><img src="http://cdn1.iconfinder.com/data/icons/flavour/button_play_blue.png" /> </div><img className="category-photo" src={film.images.image[0].src}/>{film.title}</div>)
+          return (<div className="card" key={i} style={{"color": "white"}}><div className="play"><img src="http://cdn1.iconfinder.com/data/icons/flavour/button_play_blue.png" alt="" /> </div><img className="category-photo" src={film.images.image[0].src}/>{film.title}</div>)
         }) : null}<img src={next} id="category-next" onClick={this.twoNext}/></div>
         <h3>SAMPLE CATEGORY</h3>
-        <div className="category"><img src={prev} id="category-prev" onClick={this.threePrev}/>{this.state.categoryThree ? this.state.categoryThree.map((film, i) => {
-          return (<div className="card" key={i} style={{"color": "white"}}><div className="play"><img src="http://cdn1.iconfinder.com/data/icons/flavour/button_play_blue.png" /> </div><img className="category-photo" src={film.images.image[0].src}/>{film.title}</div>)
-        }) : null}<img src={next} id="category-next" onClick={this.threeNext}/></div>
+        <div className="category"><img src={prev} id="category-prev" onClick={this.threePrev} alt="prev"/>{this.state.categoryThree ? this.state.categoryThree.map((film, i) => {
+          return (<div className="card" key={i} style={{"color": "white"}}><div className="play"><img src="http://cdn1.iconfinder.com/data/icons/flavour/button_play_blue.png" alt="" /> </div><img className="category-photo" src={film.images.image[0].src}/>{film.title}</div>)
+        }) : null}<img src={next} id="category-next" onClick={this.threeNext} alt="next"/></div>
         <footer>
           <div style={{"color":"white"}}>
-            <h4>Built with love by Ethan Lipkind</h4>
+            <h4>Built by Ethan Lipkind</h4>
           </div>
           <div>
-            <a href="https://github.com/ethantheman" style={{"color":"white", "font-family": "sans-serif"}}>
-              <img style={{"height": "50px", "width": "50px", "position": "relative"}}src={git}/>
+            <a href="https://github.com/ethantheman" style={{"color":"white", "fontFamily": "sans-serif"}}>
+              <img style={{"height": "50px", "width": "50px", "position": "relative"}} src={git} alt="github"/>
             </a>
-            <a href="https://linkedin.com/in/ethan-lipkind" style={{"color":"white", "font-family": "sans-serif"}}>
-              <img style={{"height": "50px", "width": "50px", "position": "relative"}}src={linkedin}/>
+            <a href="https://linkedin.com/in/ethan-lipkind" style={{"color":"white", "fontFamily": "sans-serif"}}>
+              <img style={{"height": "50px", "width": "50px", "position": "relative"}} src={linkedin} alt="linkedin"/>
             </a>
           </div>
         </footer>
